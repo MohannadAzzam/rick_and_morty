63b45d8591431396bf3da4bd3522ce78e82de8b4
MohannadAzzam <mohannad.azzam2000@gmail.com>
2026-03-03 17:37:30 +0200

    run the app and fix all the imports

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty/constants/my_colors.dart';
 
 class CharactersScreen extends StatelessWidget {
   const CharactersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('dksjfaklesjf ');
+    return Scaffold(
+      backgroundColor: MyColors.myGrey,
+      appBar: AppBar(
+        backgroundColor: MyColors.myYellow,
+        title: const Text('Characters'),
+        
+      ),
+      body: const Center(
+        child: Text(
+          'Characters Screen',
+          style: TextStyle(
+            color: MyColors.myWhite,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
