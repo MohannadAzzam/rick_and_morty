5ea1d2fe23970059f1767588d30d24f913bd7f34
MohannadAzzam <mohannad.azzam2000@gmail.com>
2026-03-07 17:22:43 +0200

    after error run

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,4 +1,3 @@
-// final baseUrl = 'https://www.breakingbadapi.com/api/';
 const String baseUrl = 'https://rickandmortyapi.com/api/character/';
 const charactersScreen = '/';
 const chracterDetailsScreen = '/character_details';
