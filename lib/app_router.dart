9198c718d35956999a5a82d2068a4255d49ae363
MohannadAzzam <mohannad.azzam2000@gmail.com>
2026-03-08 22:24:22 +0200

    save the streak

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'business_logic/cubit/characters_cubit.dart';
 import 'constants/strings.dart';
 import 'data/models/characters.dart';
