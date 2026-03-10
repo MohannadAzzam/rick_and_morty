a253fffc22a08256efc5e7c86b109f68ed50b0a0
MohannadAzzam <mohannad.azzam2000@gmail.com>
2026-03-10 12:37:47 +0200

    some changes

--- lib/business_logic/character_cubit/characters_cubit.dart
+++ lib/business_logic/character_cubit/characters_cubit.dart
@@ -5,8 +5,6 @@ import 'package:meta/meta.dart';
 import '../../data/models/characters.dart';
 import '../../data/repository/characters_repository.dart';
 
-// import 'package:http/http.dart' as http;
-
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
@@ -34,34 +32,3 @@ class CharactersCubit extends Cubit<CharactersState> {
     }
   }
 }
-
-
-
-  // Future<void> fetchCharacters() async {
-  //   emit(CharactersLoading());
-  //   try {
-  //     final response = await http.get(Uri.parse(baseUrl));
-
-  //     if (response.statusCode == 200) {
-  //       final characterData = characterFromJson(response.body);
-  //       emit(CharactersSuccess(characterData));
-  //     } else {
-  //       emit(CharactersError("فشل جلب البيانات من السيرفر"));
-  //     }
-  //     // final character = Character(
-  //     //   info: Info(count: 1, pages: 1, next: null, prev: null),
-  //     //   results: [
-  //     //     Result(
-  //     //       id: 1,
-  //     //       name: 'Rick Sanchez',
-  //     //       status: Status.alive,
-  //     //       species: Species.human,
-  //     //     ),
-  //     //   ],
-  //     // );
-  //     // emit(CharactersSuccess(character));
-  //   } catch (e) {
-  //     emit(CharactersError(e.toString()));
-  //   }
-  // }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'business_logic/theme_cubit/theme_cubit.dart';
-
 import 'app_router.dart';
 import 'constants/strings.dart';
 
 void main() {
-    WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     BlocProvider(
       create: (context) => ThemeCubit(),
@@ -20,22 +19,21 @@ class RickAndMorty extends StatelessWidget {
 
   const RickAndMorty({super.key, required this.appRouter});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ThemeCubit, ThemeMode>(
       builder: (context, currentTheme) {
         return MaterialApp(
-          title: 'Theme Switcher Bloc',
+          title: 'Rick and morty',
           theme: ThemeData(
             useMaterial3: true,
             appBarTheme: AppBarTheme(
               foregroundColor: Colors.white,
               backgroundColor: Colors.blueGrey,
             ),
-          ), // سمات الوضع الفاتح
-          darkTheme: ThemeData.dark(), // سمات الوضع المظلم
-          themeMode: currentTheme, // هذه هي القيمة التي تتغير من الـ Cubit
+          ),
+          darkTheme: ThemeData.dark(),
+          themeMode: currentTheme,
 
           debugShowCheckedModeBanner: false,
           onGenerateRoute: appRouter.generateRoute,
