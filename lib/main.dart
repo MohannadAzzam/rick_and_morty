cdc964661534b7ddde560f3c01fd7380e07bf8cc
MohannadAzzam <mohannad.azzam2000@gmail.com>
2026-03-10 15:18:47 +0200

    make the theme data in main for light and dark

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'app_router.dart';
 import 'constants/strings.dart';
 
 void main() {
-
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     BlocProvider(
@@ -27,13 +26,35 @@ class RickAndMorty extends StatelessWidget {
         return MaterialApp(
           title: 'Rick and morty',
           theme: ThemeData(
+            brightness: Brightness.light,
             useMaterial3: true,
-            appBarTheme: AppBarTheme(
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.blueGrey,
+            primaryColor: const Color(0xFF208D45),
+            scaffoldBackgroundColor: const Color(0xFFF0F2F5),
+            cardTheme: CardThemeData(color: Colors.white, elevation: 0),
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              foregroundColor: Color(0xFF24282F), // لون الأيقونات والنص
+              elevation: 0,
+            ),
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(color: Color(0xFF24282F)),
+              bodyMedium: TextStyle(color: Color(0xFF6E767E)),
             ),
           ),
-          darkTheme: ThemeData.dark(),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: const Color(0xFF97ce4c), // أخضر البوابة
+            scaffoldBackgroundColor: const Color(0xFF24282f), // الرمادي الرسمي
+            cardColor: const Color(0xFF3c3e44),
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Color(0xFF1e2124),
+              elevation: 0,
+            ),
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(color: Colors.white),
+              bodySmall: TextStyle(color: Color(0xFF9e9e9e)),
+            ),
+          ),
           themeMode: currentTheme,
 
           debugShowCheckedModeBanner: false,
